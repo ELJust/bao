ac21ed8cb604dddec4b5b5a71de5b4229c7df96c
Eva Lucia Justenhoven <justenhoven_eva@hotmail.de>
2018-11-14 10:36:09 +0100

    Python module where I set up the board and players at the start of the game

---
+++ BAO.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+BAO...
+"""
+
+# Set up players and board
+x = [2, 2, 2, 2, 2, 2, 2, 2, 
+     2, 2, 2, 2, 2, 2, 2, 2,]
+y = [2, 2, 2, 2, 2, 2, 2, 2, 
+     2, 2, 2, 2, 2, 2, 2, 2,]
+
+handx = 0
+handy = 0
+
+# Print status of the game
+print('Player One Board:', x)
+print('Player One Hand:', handx)
+print('Player Two Board:', y)
+print('Player Two Hand:', handy)
+
+# Move 
