fc844cb23f0ad6f0bde5eeb1091319c538dff1f6
Eva Lucia Justenhoven <justenhoven_eva@hotmail.de>
2018-11-16 09:21:10 +0100

    Defined the eat move for player 2, still haven't found out how to add value to the hand

--- BAO.py
+++ BAO.py
@@ -17,17 +17,24 @@ print('Player One Hand:', hand1)
 print('Player Two Board:', p2_board)
 print('Player Two Hand:', hand2)
 
-# Define the different moves
+# Define the empty move
 def empty_1(p1_board, hand1):
-    # Say I start at the first whole
+    # Say I start at the first hole
     # maybe try the for loop -- not working
     for number in p1_board :
         hand1 += p1_board[1] 
-    p1_board[1] = 0
+    p1_board[0] = 0
 
     return
 
-# Try out the different moves
+ # Define Eat move 
+def eat_2(p1_board, hand1):
+    # say I land in the first hole
+    if p1_board[0] >= 1 and p1_board[15] >= 1:
+        empty_1(p1_board, hand1)
+    return
+
+# Try out 
 result = empty_1(p1_board, hand1)
 print('Player One Board:', p1_board)
-print('Player One Hand:', hand1)  
+print('Player One Hand:', hand1)
