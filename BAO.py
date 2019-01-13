624ee23d3070a7897323cc42183250109b69b1aa
Eva Lucia Justenhoven <justenhoven_eva@hotmail.de>
2019-01-13 19:59:44 +0100

    Added a for loop so the game runs 50 times. Need to add saving into a csv file.

--- BAO.py
+++ BAO.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Run a game of BAO 10 times and store who won in how many turns.
+Run a game of BAO 10 times and store who won and the turn count.
 """
 from BAO_functions import game
 
 
-
-# Set up players and board
-p1_board = [2] * 16
-p2_board = [2] *16 
-hand = 0
-
 for i in range(10):
+    p1_board = [2] * 16
+    p2_board = [2] * 16 
+    hand = 0
     game(p1_board, p2_board)
+    # store/append result in a csv file
 
 # To Do:     
 #define strategies
-# store result
-
-       
